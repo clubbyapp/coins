cee3dde235a1de4992043d5a74ce605af16b1672
Tobias Kaulfuß <tobias.kaulfuss@denkwerk.com>
2017-12-23 11:32:04 +0100

    controller extended

--- src/main/java/com/cryptoboard/coins/controller/MainController.java
+++ src/main/java/com/cryptoboard/coins/controller/MainController.java
@@ -12,12 +12,24 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "/getData", method = RequestMethod.GET)
+    @RequestMapping(value = "/importData", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public void importData(Model model) {
         RestTemplate restTemplate = new RestTemplate();
         String coinmarketcapApiUrl = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR";
         Coin[] coins = restTemplate.getForObject(coinmarketcapApiUrl, Coin[].class);
+        double btcPriceEur = 0;
+        double ethPriceEur = 0;
+        for (Coin coin : coins) {
+            if ("bitcoin".equals(coin.getId())) {
+                btcPriceEur = coin.getPrice_eur();
+            } else if ("ethereum".equals(coin.getId())) {
+                ethPriceEur = coin.getPrice_eur();
+            }
+        }
+        System.out.println("BTC: " + 0.15 * btcPriceEur);
+        System.out.println("ETH; " + 2.3 * ethPriceEur);
+
         return;
     }
 }
